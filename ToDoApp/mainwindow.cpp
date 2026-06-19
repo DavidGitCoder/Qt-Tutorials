8b1c189fc1cd72e6af4df4710d41f88bb8780f0c
DavidGitCoder <github@solutionzmail.com>
2026-06-19 14:31:28 +0200

    feat/file upload

--- ToDoApp/mainwindow.cpp
+++ ToDoApp/mainwindow.cpp
@@ -1,14 +1,31 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFile>
+#include <QStandardPaths>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // Try to open the toDoFile.txt and populate the list if it does
+    QFile file(QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation) + "\\toDoFile.txt");
+    if(!file.open(QIODevice::ReadWrite)){
+        QMessageBox::information(0,"error", file.errorString());
+    }
+    QTextStream in(&file);
+
+    while(!in.atEnd()){
+        QListWidgetItem* item=new QListWidgetItem(in.readLine(), ui->listWidget);
+        ui->listWidget->addItem(item);
+        item->setFlags(item->flags() | Qt::ItemIsEditable);
+    }
+    file.close();
 }
 
-MainWindow::~MainWindow()
+MainWindow::~MainWindow() // destructor
 {
     delete ui;
 }
@@ -18,15 +35,18 @@ void MainWindow::on_btnAdd_clicked()
 
     QListWidgetItem* item=new QListWidgetItem(ui->txtTask->text(),ui->listWidget);
     ui->listWidget->addItem(item);
+    item->setFlags(item->flags() | Qt::ItemIsEditable);
+    ui->txtTask->clear();
     ui->txtTask->setFocus();
+
 
 }
 
 
 void MainWindow::on_btnRemove_clicked()
 {
-    int rowItem=ui->listWidget->currentRow();
-    ui->listWidget->takeItem(rowItem);
+    QListWidgetItem* item=ui->listWidget->takeItem(ui->listWidget->currentRow());
+    delete item;
 }
 
 
